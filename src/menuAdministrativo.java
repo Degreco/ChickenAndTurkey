02eba7980a698ba6217ce77e322feedf11bbad86
Antoni Coll Reynés <acollreynes@hotmail.com>
2023-05-12 17:48:03 +0200

    Creación opciones de menu administrativo
    En este apartado se ha creado en Menú Administrativo con varios opciones.

---
+++ src/menuAdministrativo.java
@@ -0,0 +1,15 @@
+
+public class menuAdministrativo {
+
+	public static void menuPrincipalAdministrativo(){
+		
+		System.out.println("******* MENU ADMINISTRATIVO *******");
+		System.out.println("");
+		System.out.println("Consultar usuarios");
+		System.out.println("Consultar vehículos");
+		
+		
+		
+	}
+
+}
