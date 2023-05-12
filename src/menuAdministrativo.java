c363db2527c8c3e80b8353a60ca94d47d3bb1508
Pons <alanponstrabajo@gmail.com>
2023-05-12 18:58:41 +0200

    Probando

--- src/menuAdministrativo.java
+++ src/menuAdministrativo.java
@@ -7,8 +7,7 @@ public class menuAdministrativo {
 		System.out.println("");
 		System.out.println("Consultar usuarios");
 		System.out.println("Consultar vehículos");
-		
-		
+		System.out.println("Probando cosas");
 		
 	}
 
