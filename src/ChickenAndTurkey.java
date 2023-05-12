7bb2b2283a321082fc9725afcb21965023d98bee
Degreco <antoniodaniel@estudiante.edib.es>
2023-05-12 18:40:48 +0200

    db Login

--- src/ChickenAndTurkey.java
+++ src/ChickenAndTurkey.java
@@ -4,18 +4,21 @@ import java.sql.SQLException;
 
 public class ChickenAndTurkey {
 
-	public static void main(String[] args) {
-		String username = "admin";
-		String password = "chicken123";
-		String urlBD = "jdbc:mysql://localhost:3306/chickenandturkey";
-		try {
-			Connection conn = DriverManager.getConnection(urlBD, username, password);
-			System.out.println("Cargado correctamente");
-			System.out.println("Conexión exitosa a la base de datos chickenandturkey.");
-			conn.close();
-		} catch (SQLException e) {
-			System.out.println("Error al conectar a la base de datos: " + e.getMessage());
-		}
+    public static void main(String[] args) {
+        String username = "admin";
+        String password = "chicken123";
+        String urlBD = "jdbc:mysql://localhost:3306/chickenandturkey";
+        try {
+            Connection conn = DriverManager.getConnection(urlBD, username, password);
+            System.out.println("Cargado correctamente");
+            System.out.println("Conexión exitosa a la base de datos chickenandturkey.");
+            conn.close();
 
-	}
+            dbLogin login = new dbLogin(urlBD, username, password);
+            login.dbLogin(); // Call dbLogin method
+        } catch (SQLException e) {
+            System.out.println("Error al conectar a la base de datos: " + e.getMessage());
+        }
+
+    }
 }
