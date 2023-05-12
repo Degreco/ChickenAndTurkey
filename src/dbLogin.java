61eaa6cb4b118d144dc2a5dfd94f9563413a7be3
Degreco <antoniodaniel@estudiante.edib.es>
2023-05-12 18:23:57 +0200

    Git ignore
    Preventing conflicts between our configuration files.

--- src/dbLogin.java
+++ src/dbLogin.java
@@ -1,4 +1,13 @@
+import java.util.Scanner;
 
 public class dbLogin {
 
+	public dbLogin() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Por favor introduce ");
+		String userInput = sc.nextLine();
+	}
+	
+	
+	
 }
